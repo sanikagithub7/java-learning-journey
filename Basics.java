9a6bd551ca54a5c19243a89142f7b147f000f69c
Sanika Wazarkar <wazarkarsanika20@gmail.com>
2026-06-24 03:52:36 +0530

    Day 1 - Java variables and data types

---
+++ Basics.java
@@ -0,0 +1,16 @@
+public class Basics {
+    public static void main(String[] args) {
+
+        int age = 20;
+        double height = 5.5;
+        char grade = 'A';
+        boolean isStudent = true;
+        String name = "Sanika";
+
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Height: " + height);
+        System.out.println("Grade: " + grade);
+        System.out.println("Student: " + isStudent);
+    }
+}
